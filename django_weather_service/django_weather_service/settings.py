f8e9f8c6f5871b7af449abb82e6bf83e47b8f0ac
Albert <118181873+Terrasd@users.noreply.github.com>
2023-08-29 16:55:00 +0700

    Update settings.py

--- django_weather_service/django_weather_service/settings.py
+++ django_weather_service/django_weather_service/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#j5!&c26i=7k!-=0^xggo25&mm3+so!6nzgz58v9ay(5b_fjq^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
