0f719dda6ce38af409cf4d7567556b648cd41d2e
Terrasd <Shestakov_04@mail.ru>
2023-08-29 10:53:32 +0700

    V_1.0

---
+++ main.py
@@ -0,0 +1,13 @@
+import func
+from formating_strings import Color
+
+cities = [
+    "Москва",
+    "Красноярск",
+    "Новосибирск",
+    "Томск"
+]
+
+for city in cities:
+    print(f"""Погода в городе {Color.BOLD + Color.CYAN + city + Color.END}: 
+{func.what_weather(city)}""")
