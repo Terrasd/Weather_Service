7091a16dad03d490442ae290f41cbdb73a942af1
Terrasd <Shestakov_04@mail.ru>
2023-08-29 15:36:45 +0700

    Adding_for_django_project

--- func.py
+++ func.py
@@ -2,16 +2,16 @@ import requests
 
 
 def what_weather(city):
-    url = f"http://wttr.in/{city}"
+    url = f'http://wttr.in/{city}'
     weather_parameters = {
-        "format": 2,
-        "M": "",
+        'format': 2,
+        'M': '',
     }
     try:
         response = requests.get(url, params=weather_parameters)
     except requests.ConnectionError:
-        return "<сетевая ошибка>"
+        return '<сетевая ошибка>'
     if response.status_code == 200:
         return response.text
     else:
-        return "<ошибка на сервере погоды>"
+        return '<ошибка на сервере погоды>'
--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import func
 from formating_strings import Color
 
 cities = [
-    "Москва",
-    "Красноярск",
-    "Новосибирск",
-    "Томск"
+    'Москва',
+    'Красноярск',
+    'Новосибирск',
+    'Томск'
 ]
 
 for city in cities:
-    print(f"""Погода в городе {Color.BOLD + Color.CYAN + city + Color.END}: 
-{func.what_weather(city)}""")
+    print(f'''Погода в городе {Color.BOLD + Color.CYAN + city + Color.END}: 
+{func.what_weather(city)}''')
